56f81c38408af0126d361240afac8610af910b23
TytkoJakub <tytko.jakub@gmail.com>
2021-01-21 20:02:44 +0100

    Database relation correct

---
+++ PortfolioLabCharity/src/Charity/Charity_1/Models/DbModels/DonationCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charity.Models.DbModels
+{
+    public class DonationCategory
+    {
+        [Key]
+        public string Id { get; set; }
+        public string DonationId { get; set; }
+        public string CategoryId { get; set; }
+        //Relationships
+        [ForeignKey("DonationId")]
+        public Donation Donation { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+}
